5d43e8defb9ee6949fdb7d726ec8d017a1f94a3f
Andre Lukas Schorlemmer <andre.schorlemmer@gmail.com>
2017-09-27 17:22:01 -0700

    Custom passwordless auth backend + custom user model

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from lists import views as list_views
 from lists import urls as list_urls
+from accounts import urls as list_accounts
 
 urlpatterns = [
     url(r"^$", list_views.home_page, name="home"),
     url(r"^lists/", include(list_urls)),
+    url(r"^accounts/", include(list_accounts)),
 ]
